37c2d1da381e8d9b728b15aea21ffd7536de4584
Droid-DevX <ayushtando@gmail.com>
2026-03-18 17:05:50 +0530

    fix: added base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/Resturant/", 
+
   plugins: [react()],
+  server: {
+    host: '[IP ADDRESS]',
+    port: 5173,
+  },
 });
